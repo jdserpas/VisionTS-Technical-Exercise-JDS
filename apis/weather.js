8bc07e09932a85a0c301b8565e3277a7230d21fd
jdserpas <jdserpas@gmail.com>
2021-08-19 22:18:30 -0500

    added weather API functionality

---
+++ apis/weather.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const keys = require('../keys');
+
+/* function that accepts a String and creates a url to query the weather API */
+const makeUrl = (city) => {
+    if (city === undefined || city === '') {
+      return;  
+    }
+
+    // Clearing spaces in case city has multiple words eg: New York
+    const httpCity = city.replace(" ", "%20");
+
+    /* Construct the url with:
+        City: based on user passed args
+        Units: will be given in imperial
+        AppId: is the Token Key
+    */
+    return `http://api.openweathermap.org/data/2.5/weather?q=${httpCity}&units=imperial&appid=${keys.weather}`
+};
+
+/*  Argument:  
+    [Type: STRING] City
+        City to be sent to API
+    [Type: FUNCTION] fn(weather)
+        Callback function to be called once the API call completes
+        Argument:
+        [Type: OBJ] weather
+            JSON compliant object contaning longitude, latitude, and weather
+*/
+const call = (city, fn) => {
+
+    const url = makeUrl(city);
+
+    if(url === undefined || url === '') {
+        let error = {};
+        error.err = true;
+        error.code = 400;
+        error.message = "City was not provided or was formatted incorrectly. Please check documentation";
+        return error;
+    }
+    
+    /* Make API request */
+    http.get(url, (res)=>{
+        res.on('data', (data)=>{
+            obj = JSON.parse(data);
+            
+            let weather = {};
+            weather.lon = obj.coord.lon;
+            weather.lat = obj.coord.lat;
+            weather.temp = obj.main.temp;
+            fn(weather);
+        });
+    });
+};
+
+module.exports = {
+    call
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const weather = require('../apis/weather');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET weather API */
+router.get('/weather', (req, res) => {
+  const city = req.query.city;
+
+  if (city === undefined || city === '') {
+    res.send('You must provide a city\n');
+  }
+  
+  weather.call(city, (data) => {
+    res.json(data);
+  })
 });
 
 module.exports = router;
