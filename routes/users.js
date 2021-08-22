ad3bea50b049046566463a275c57f222edd4f309
jdserpas <jdserpas@gmail.com>
2021-08-22 00:20:17 -0500

    Excercise 3 & 4

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ctrlUsers = require('../controllers/users');
 
-/* GET users listing. */
+/* GET users from db. */
+/* Completes exercise 4 */
 router.get('/', ctrlUsers.getUser);
 
+/* POST users to db. */
+/* Completes exercise 3 */
 router.post('/', ctrlUsers.createUser);
 
 module.exports = router;
